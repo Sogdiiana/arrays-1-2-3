ec9668d791cfae46b3f29ab9536de87f5f22a812
Sogdiana <sogdianaravshanova@mail.ru>
2023-02-15 15:37:18 +0500

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int massive[] = { 3,6,7,59,765,3456,3557,90900,34,23,789,345,3,21,5,9856,889 };
+	for (int i = 0; i < 17; i++)
+		cout << massive[i] << endl;
+
+	return 0; 
+}
